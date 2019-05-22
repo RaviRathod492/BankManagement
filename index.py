1f03da8f2cc6491d5a1107c8dbe775ae9d296685
Bhavesh <bparvatkar387@rku.ac.in>
2019-05-22 17:21:40 +0530

    1. Added switch case for the 4 options 2. Added the myFunc file to update it later

--- index.py
+++ index.py
@@ -1,3 +1,5 @@
+import myFunc
+
 print("|{:-^50}|".format(""))
 print("|{:^50}|".format("Welcome"))
 print("|{:-^50}|".format(""))
@@ -7,4 +9,16 @@ print("|{:50}|".format("  2. Signin"))
 print("|{:50}|".format("  3. Admin Signin"))
 print("|{:50}|".format("  4. Quit"))
 print("|{:-^50}|".format(""))
-print("|{:50}|".format("Enter your choice: "))
+
+__ch = input("Enter your choice: ")
+
+if __ch == "1":
+    myFunc.signup()
+elif __ch == "2":
+    myFunc.login()
+elif __ch == "3":
+    myFunc.admin()
+elif __ch == "4":
+    exit()
+else:
+    print("You entered an incorrect choice. Please try again!")
---
+++ myFunc.py
@@ -0,0 +1,10 @@
+def signup():
+    print("Will create signup form here")
+
+
+def login():
+    print("Will create login form here")
+
+
+def admin():
+    print("Will create admin login here")
